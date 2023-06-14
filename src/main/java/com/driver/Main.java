c51ed6e5bb85ce931dbad4ea67103ea60a647a67
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-06-14 16:18:02 +0530

    Solution Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+
+//    a.RWOnly = "Puneeth"; not possible because it is private;
+        a.setName("Puneeth");
+        a.getName();
+//        System.out.println(a.getName());
+    }
 }
